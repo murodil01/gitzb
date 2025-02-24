39d06ad43a36f88cf532524bcdc462d3d96c5afa
murodil01 <nurmamatovmirodil978@gmail.com>
2025-02-24 18:36:14 +0500

    ozgardi

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,22 @@
-import { useState } from 'react'
-import "./App.css";
+//import { useState } from 'react'
+//import "./App.css";
 //import HeaderHero from "./Components/HeaderHero";
 //import Cards from "./Components/Cards";
 //import Retspet from "./Components/Retsept";
 //import Map from './Components/Map';
 //import Footer from "./Components/Footer";
-
+{/*
 function App() {
   return (
     <>
-      {/*<HeaderHero />
+      <HeaderHero />
       <Cards />
       <Retspet />
       <Map/>
-      <Footer />*/}
+      <Footer />
 
     </>
   );
 }
 
-export default App;
+export default App;*/}
